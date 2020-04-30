9a17ffb842d0f71e24d58c78b19d6b3ab9d2fa22
Karl Liu <karl.liu.1024@gmail.com>
2020-04-30 15:09:39 +0800

    succeeded in convert an CoreImage instance into GtkImage instance

--- gui/main.c
+++ gui/main.c
@@ -5,15 +5,38 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 #include "main.h"
+#include "../core/image.h"
 
 int gui_main(int argc, char **argv) {
     GtkWidget *window;
+    GtkWidget *image_widget;
+    CoreImage *image;
+    CoreSize *size;
+    guint8 pix[3] = {255, 0, 0};
+    GdkPixbuf *pixbuf;
+    gsize i, j;
+
     gtk_init(&argc, &argv);
+
+    size = core_size_new_with_value(640, 480);
+    image = core_image_new_fill_with_color(size, 24, pix);
+    pixbuf = gdk_pixbuf_new(GDK_COLORSPACE_RGB, FALSE, 8, core_size_get_width(size), core_size_get_height(size));
+    for (i = 0; i < core_size_get_area(size); ++i) {
+        for (j = 0; j < 3; ++j) {
+            // note: No information about how pixel data are stored in pixbuf instance provided.
+            // I guess it'll work and it does work for now.
+            gdk_pixbuf_get_pixels(pixbuf)[i * 3 + j] = core_image_get_data(image)[i * 3 + j];
+        }
+    }
+    image_widget = gtk_image_new_from_pixbuf(pixbuf);
+
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
     gtk_window_set_title(GTK_WINDOW(window), "EasyPhotoshop");
-    gtk_window_set_default_size(GTK_WINDOW(window), 230, 150);
+    gtk_window_set_default_size(GTK_WINDOW(window), 640, 480);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-    gtk_widget_show(window);
+    gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(image_widget));
+    gtk_widget_show_all(window);
 
     g_signal_connect_swapped(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     gtk_main();
--- main.c
+++ main.c
@@ -1,41 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "core/image.h"
-
-#define print_bool(expr) printf((expr) ? "true\n" : "false")
+#include "gui/main.h"
 
 int main(int argc, char **argv) {
-    CoreImage *img = 0;
-    CoreSize *size = 0;
-    guint8 pix[3] = {1, 2, 3};
-    guint8 bpp = 24;
-    GString *str = 0;
-    gsize i, j, k;
-
-    size = core_size_new_with_value(5, 4);
-    img = core_image_new_fill_with_color(size, bpp, pix);
-    g_object_unref(size);
-    size = NULL;
-
-    core_image_get_size(img, &size);
-    core_size_to_string(size, &str);
-    printf("%s\n", str->str);
-    for (i = 0; i < core_size_get_area(size); ++i) {
-        for (j = 0; j < (bpp >> 3u); ++j) {
-            printf("%d, ", (gint32) core_image_get_data(img)[i * (bpp >> 3u) + j]);
-        }
-    }
-    printf("\n");
-    for (i = 0; i < core_size_get_height(size); ++i) {
-        for (j = 0; j < core_size_get_width(size); ++j) {
-            printf("[");
-            for (k = 0; k < core_image_get_byte_per_pixel(img); ++k) {
-                if (k != 0) printf(" ");
-                printf("%d", (gint32) core_image_get_pixel(img, j, i)[k]);
-            }
-            printf("], ");
-        }
-        printf("\n");
-    }
-    return EXIT_SUCCESS;
+    return gui_main(argc, argv);
 }
