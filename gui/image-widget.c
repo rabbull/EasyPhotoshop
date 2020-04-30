87e31df2ee19ead8605ab2963529c7c7acb43fc8
Karl Liu <karl.liu.1024@gmail.com>
2020-04-30 17:34:05 +0800

    gui image widget class added

---
+++ gui/image-widget.c
@@ -0,0 +1,49 @@
+//
+// Created by karl_ on 2020-4-30.
+//
+
+#include "image-widget.h"
+
+G_DEFINE_TYPE(GuiImageWidget, gui_image_widget, GTK_TYPE_IMAGE)
+
+static void gui_image_widget_class_init (GuiImageWidgetClass *cls) {}
+
+static void gui_image_widget_init (GuiImageWidget *self) {}
+
+GuiImageWidget* gui_image_widget_new_from_core_image(CoreImage* image) {
+    GuiImageWidget* image_widget = NULL;
+    CoreSize *size = 0;
+    GdkPixbuf *pixbuf;
+    gsize i, j, area;
+    guint8 byte_per_pixel;
+
+    // increase reference
+    image = g_object_ref(image);
+
+    core_image_get_size(image, &size);
+    pixbuf = gdk_pixbuf_new(GDK_COLORSPACE_RGB, FALSE, 8, core_size_get_width(size), core_size_get_height(size));
+    area = core_size_get_area(size);
+    byte_per_pixel = core_image_get_byte_per_pixel(image);
+    for (i = 0; i < area; ++i) {
+        for (j = 0; j < byte_per_pixel; ++j) {
+            // note: No information about how pixel data are stored in pixbuf instance provided.
+            // I guess it'll work and it does work for now.
+            gdk_pixbuf_get_pixels(pixbuf)[i * 3 + j] = core_image_get_data(image)[i * 3 + j];
+        }
+    }
+    image_widget = g_object_new(GUI_TYPE_IMAGE_WIDGET, NULL);
+    gtk_image_set_from_pixbuf(GTK_IMAGE(image_widget), pixbuf);
+
+    // decrease reference
+    g_object_unref(image);
+    g_object_unref(pixbuf);
+    return image_widget;
+}
+
+static void gui_image_widget_dispose (GObject *obj) {
+    G_OBJECT_CLASS (gui_image_widget_parent_class)->dispose (obj);
+}
+
+static void gui_image_widget_finalize (GObject *obj) {
+    G_OBJECT_CLASS (gui_image_widget_parent_class)->finalize (obj);
+}
---
+++ gui/image-widget.h
@@ -0,0 +1,26 @@
+//
+// Created by karl_ on 2020-4-30.
+//
+
+#ifndef EASYPHOTOSHOP_IMAGE_WIDGET_H
+#define EASYPHOTOSHOP_IMAGE_WIDGET_H
+
+#include <glib-object.h>
+#include <gtk/gtk.h>
+#include "../core/image.h"
+
+G_BEGIN_DECLS
+
+#define GUI_TYPE_IMAGE_WIDGET gui_image_widget_get_type()
+G_DECLARE_DERIVABLE_TYPE(GuiImageWidget, gui_image_widget, GUI, IMAGE_WIDGET, GtkImage)
+
+struct _GuiImageWidgetClass {
+    GtkImageClass parent_class;
+    gpointer padding[12];
+};
+
+GuiImageWidget* gui_image_widget_new_from_core_image(CoreImage* image);
+
+G_END_DECLS
+
+#endif //EASYPHOTOSHOP_IMAGE_WIDGET_H
