80f52c33b8f09f61c1596c2f8453fdc78b5050ea
Karl Liu <karl.liu.1024@gmail.com>
2020-04-30 19:58:09 +0800

    welcome lena

---
+++ core/lena.h
@@ -0,0 +1,12 @@
+//
+// Created by karl_ on 2020-4-30.
+//
+
+#ifndef EASYPHOTOSHOP_LENA_H
+#define EASYPHOTOSHOP_LENA_H
+
+#include "image.h"
+
+CoreImage* core_image_new_lena();
+
+#endif //EASYPHOTOSHOP_LENA_H
--- gui/main.c
+++ gui/main.c
@@ -5,27 +5,28 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 #include "main.h"
-#include "../core/image.h"
 #include "image-widget.h"
+#include "../core/image.h"
+#include "../core/lena.h"
 
 int gui_main(int argc, char **argv) {
     GtkWidget *window;
     GuiImageWidget *gui_image_widget;
     CoreImage *image;
-    CoreSize *size;
+    CoreSize *size = 0;
     guint8 pix[3] = {255, 0, 0};
 
     gtk_init(&argc, &argv);
 
-    size = core_size_new_with_value(640, 480);
-    image = core_image_new_fill_with_color(size, 24, pix);
+    image = core_image_new_lena();
+    core_image_get_size(image, &size);
     gui_image_widget = GUI_IMAGE_WIDGET(gui_image_widget_new_from_core_image(image));
     g_object_unref(image);
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 
     gtk_window_set_title(GTK_WINDOW(window), "EasyPhotoshop");
-    gtk_window_set_default_size(GTK_WINDOW(window), 640, 480);
+    gtk_window_set_default_size(GTK_WINDOW(window), 100, 100);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(gui_image_widget));
     gtk_widget_show_all(window);
