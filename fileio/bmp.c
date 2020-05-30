c396f9603ac0548659fb67bfadf8061ee4130147
Karl Liu <karl.liu.1024@gmail.com>
2020-05-30 18:05:14 +0800

    seperated wrapper function and do function

--- fileio/bmp.c
+++ fileio/bmp.c
@@ -6,11 +6,22 @@
 
 static CoreImage *bmp_load(GString *path);
 
+static void do_bmp_load(char const *path, void **buffer);
+
 void bmp_init(FileIOInputFormatTable *table) {
     fileio_input_format_table_register(table, bmp_load);
 }
 
 static CoreImage *bmp_load(GString *path) {
-    /* always return false until being implemented */
-    return FALSE;
+    CoreImage *image = NULL;
+    void *buffer = NULL;
+    do_bmp_load(path->str, &buffer);
+
+    /* TODO: convert buffer into CoreImage */
+    /* always return NULL until being implemented */
+    return image;
+}
+
+static void do_bmp_load(char const *const path, void **buffer) {
+    /* TODO: implement it */
 }
