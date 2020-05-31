dd2b9c6119a3f8d20a59b63f14f87f6e25a20688
Karl Liu <karl.liu.1024@gmail.com>
2020-05-31 19:42:40 +0800

    move callbacks to seperated files

---
+++ gui/callbacks.h
@@ -0,0 +1,16 @@
+//
+// Created by karl on 31/5/2020.
+//
+
+#ifndef EASYPHOTOSHOP_CALLBACKS_H
+#define EASYPHOTOSHOP_CALLBACKS_H
+
+#include <gtk/gtk.h>
+
+void grayscale(GtkWidget *widget, gpointer data);
+
+void histeq(GtkWidget *widget, gpointer data);
+
+void open_file(GtkWidget *widget, gpointer data);
+
+#endif //EASYPHOTOSHOP_CALLBACKS_H
--- gui/main.c
+++ gui/main.c
@@ -6,43 +6,9 @@
 #include <gtk/gtk.h>
 #include <gui/image-widget.h>
 #include <core/lena.h>
-#include <imgproc/color-convert.h>
-#include <imgproc/histogram-equalization.h>
 
-void grayscale(GtkWidget *widget, gpointer data) {
-    GuiImageWidget *img_widget = GUI_IMAGE_WIDGET(data);
-    CoreImage *image = gui_image_widget_get_image(img_widget);
-    imgproc_to_grayscale(image, &image);
-    gui_image_widget_update_image(img_widget, image);
-    g_object_unref(image);
-}
-
-void histeq(GtkWidget *widget, gpointer data) {
-    GuiImageWidget *img_widget = GUI_IMAGE_WIDGET(data);
-    CoreImage *image = gui_image_widget_get_image(img_widget);
-    imgproc_histogram_equalization(image, &image);
-    gui_image_widget_update_image(img_widget, image);
-    g_object_unref(image);
-}
-
-void open_file(GtkWidget *widget, gpointer data) {
-    GtkWidget *dialog;
-    dialog = gtk_file_chooser_dialog_new("open file", NULL, GTK_FILE_CHOOSER_ACTION_OPEN, "Cancel", GTK_RESPONSE_CANCEL,
-                                         "Open", GTK_RESPONSE_ACCEPT, NULL);
-    GtkFileFilter *filter;
-    filter = gtk_file_filter_new();
-    gtk_file_filter_set_name(filter, "bmp");
-    gtk_file_filter_add_pattern(filter, "*.bmp");
-    gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dialog), filter);
-    if (gtk_dialog_run(GTK_DIALOG (dialog)) == GTK_RESPONSE_ACCEPT) {
-        GFile *file;
-        file = gtk_file_chooser_get_file(GTK_FILE_CHOOSER (dialog));
-        /* do something */
-        g_object_unref(file);
-    }
-    gtk_widget_destroy(dialog);
-}
-
+#include "main.h"
+#include "callbacks.h"
 
 int gui_main(int argc, char **argv) {
     GtkWidget *window, *box_main, *box_buttons;
