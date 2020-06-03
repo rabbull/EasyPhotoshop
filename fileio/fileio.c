03243f7b6a875f533c9725df57c8e3a37ed12826
Karl Liu <karl.liu.1024@gmail.com>
2020-06-03 15:47:45 +0800

    add init of output submodule into the init of fileio module

--- fileio/fileio.c
+++ fileio/fileio.c
@@ -6,15 +6,21 @@
 #include "bmp.h"
 
 void fileio_init() {
-    FileIOInputFormatTable *table;
-    table = fileio_input_format_table_get_instance();
-    bmp_init(table, NULL);
-    g_object_unref(table);
+    FileIOInputFormatTable *input_table;
+    FileIOOutputFormatTable *output_table;
+    input_table = fileio_input_format_table_get_instance();
+    output_table = fileio_output_format_table_get_instance();
+    bmp_init(input_table, output_table);
+    g_object_unref(input_table);
+    g_object_unref(output_table);
 }
 
 void fileio_exit() {
     FileIOInputFormatTable *table;
+    FileIOOutputFormatTable *output_table;
     table = fileio_input_format_table_get_instance();
-    bmp_exit(table, NULL);
+    output_table = fileio_output_format_table_get_instance();
+    bmp_exit(table, output_table);
     fileio_input_format_table_release(table);
+    fileio_output_format_table_release(output_table);
 }
