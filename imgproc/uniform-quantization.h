878f4ceda36239a79d782f95283982ca404179d0
Mr9567 <1079626506@qq.com>
2020-06-06 18:33:45 +0800

    均匀量化

---
+++ imgproc/uniform-quantization.h
@@ -0,0 +1,11 @@
+//
+// Created by Mr.W on 2020/6/6.
+//
+
+#ifndef EASYPHOTOSHOP_UNIFORM_QUANTIZATION_H
+#define EASYPHOTOSHOP_UNIFORM_QUANTIZATION_H
+#include <stdint.h>
+#include <core/image.h>
+
+CoreImage *imgproc_uniform_quantization(CoreImage* image, guint32 ratio);
+#endif //EASYPHOTOSHOP_UNIFORM_QUANTIZATION_H
