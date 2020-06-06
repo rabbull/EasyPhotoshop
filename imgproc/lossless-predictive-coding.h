0685ba41230ad2992ed1441aacf0031c63781a15
Karl Liu <karl.liu.1024@gmail.com>
2020-06-06 14:43:47 +0800

    reformat

--- imgproc/lossless-predictive-coding.h
+++ imgproc/lossless-predictive-coding.h
@@ -5,9 +5,9 @@
 #ifndef EASYPHOTOSHOP_LOSSLESS_PREDICTIVE_CODING_H
 #define EASYPHOTOSHOP_LOSSLESS_PREDICTIVE_CODING_H
 
-#endif //EASYPHOTOSHOP_LOSSLESS_PREDICTIVE_CODING_H
-
 #include <stdint.h>
 #include <core/image.h>
 
-CoreImage *imgproc_lossless_predictive_coding(CoreImage *image, guint32 rank, guint *coefficient);
+CoreImage *imgproc_lossless_predictive_coding(CoreImage *image, guint32 rank, gdouble *coefficient);
+
+#endif //EASYPHOTOSHOP_LOSSLESS_PREDICTIVE_CODING_H
